9b29ff532ee617435aafb26da30b9121a1600f5c
宋超波 <974404651@qq.com>
2019-10-11 17:25:36 +0800

    第一版

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp");
+
+//监听任务
+gulp.task("watchall", async ()=>{
+	//监听html,进行复制
+	gulp.watch("*.html",async ()=>{
+      gulp.src("*.html")
+	  .pipe(gulp.dest("D:\\phpStudy\\WWW\\tongzhen"));
+    });
+
+})
